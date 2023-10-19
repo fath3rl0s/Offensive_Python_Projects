f71f1620edc511b5bb4b875f60bf978f28266a75
Carlos Enamorado <88733190+fath3rl0s@users.noreply.github.com>
2023-10-18 21:02:31 -0400

    Update 2. offsetHelp.py

--- Vuln Server/Buffer Overflow/2. offsetHelp.py
+++ Vuln Server/Buffer Overflow/2. offsetHelp.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-# Carlos Enamorado
-# 10/17/2023
-# Finding the Offset after fuzzing
-# You will need to leverage pattern_create.rb before using this script
-# Take the byte size found in fuzzer.py and load into '~/pattern_create.rb -l <fuzzer.py_results>'
-# Then run this program to find the pattern that lands in the EIP with Immunity Debugger
-# After all this, load the pattern you have for EIP into '~/pattern_offset.rb -l <fuzzer.py_results>  -q <EIP_pattern>'
-# The result is the OFFSET!
 
+'''
+Carlos Enamorado
+10/17/2023
+Finding the Offset after fuzzing
+You will need to leverage pattern_create.rb before using this script
+Take the byte size found in fuzzer.py and load into '~/pattern_create.rb -l <fuzzer.py_results>'
+Then run this program to find the pattern that lands in the EIP with Immunity Debugger
+After all this, load the pattern you have for EIP into '~/pattern_offset.rb -l <fuzzer.py_results>  -q <EIP_pattern>'
+The result is the OFFSET!
+In the case of VulnServer and for the purpose of these scripts moving forward, this is '2003'
+'''
 
 import sys, socket
 from time import sleep
